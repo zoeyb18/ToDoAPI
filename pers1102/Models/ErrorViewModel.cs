d6a4ed5d51bfe6b646ac7a7a3abe875c6bc87197
Zoey <135077398+zoeyb18@users.noreply.github.com>
2023-11-17 13:50:05 -0600

    Delete pers1102 directory

--- pers1102/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace pers1102.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
